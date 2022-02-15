9c5295a36407f4b204b15ee07ecc6367892ec39c
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-15 18:41:18 -0300

    Version Usuario

--- src/Empresa/Main.java
+++ src/Empresa/Main.java
@@ -1,10 +1,38 @@
 package Empresa;
 
+import Git.Region;
+import Git.Usuario;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+
+}
+class Empresa {
+
+	private Usuario usuario;
+	private Region region;
+	
+	
+	public Empresa(Usuario usuario, Region region) {
+		this.usuario = usuario;
+        this.region = region;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public Region getRegion() {
+		return region;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa ( " + getUsuario() + ", " + getRegion() + " )";
+	}
+	
 
 }

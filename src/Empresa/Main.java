3eee3155163cb5e783eb93dbe1559ecea776f48c
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-15 21:25:08 -0300

    Ultima version

--- src/Empresa/Main.java
+++ src/Empresa/Main.java
@@ -1,22 +1,74 @@
 package Empresa;
 
-
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Usuario persona = new Usuario("Alegre", "Lucas");
+		Region localidad = new Region("Concordia, Entre ríos, Argentina");
+		Empresa empresa = new Empresa(persona, localidad);
+		
+		System.out.println("-----------------Bienvenidos------------------");
+		System.out.println(empresa.toString());
+		System.out.println("-----------------Bienvenidos------------------");
 
 	}
+}
 
+class Empresa {
+
+	private Usuario usuario;
+	private Region region;
+
+	public Empresa(Usuario usuario, Region region) {
+		this.usuario = usuario;
+		this.region = region;
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public Region getRegion() {
+		return region;
+	}
+	@Override
+	public String toString() {
+		return "Empresa ( " + getUsuario() + ", " + getRegion() + " )";
+	}
 
 }
-class Region{
+
+class Usuario {
+
+	private String apellido;
+	private String nombre;
+
+	public Usuario(String apellido, String nombre) {
+		this.apellido = apellido;
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario( " + getApellido() + ", " + getNombre() + " )";
+	}
+	
+}
+
+
+class Region {
 
 	private String localidad;
-	public Region(String localidad){
-	this.localidad = localidad;
 
+	public Region(String localidad) {
+		this.localidad = localidad;
 	}
+
 	public String getLocalidad() {
 		return localidad;
 	}
@@ -24,8 +76,5 @@ class Region{
 	public String toString() {
 		return "Region [localidad=" + localidad + "]";
 	}
-	
-	
+
 }
-
-
